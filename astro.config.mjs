375d589f88885e4f9dc6be7309a58e6e1df86ffd
Nikita <nikitaserg762@gmail.com>
2026-06-08 16:56:34 +0300

    Изменен URL сайта в конфигурации на 'https://vladimir.vindfjur.art'

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import keystatic from '@keystatic/astro';
 const withCMS = process.argv.includes('dev');
 
 export default defineConfig({
-  site: 'https://vladimir33.vercel.app',
+  site: 'https://vladimir.vindfjur.art',
   trailingSlash: 'ignore',
   build: { format: 'directory' },
   devToolbar: { enabled: false },
